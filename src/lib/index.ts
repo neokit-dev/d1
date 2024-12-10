f78eb57295f9a4bbe13bcf1dccd88e652df4e774
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-12-10 19:24:18 +0100

    d1 v1

---
+++ src/lib/index.ts
@@ -0,0 +1,44 @@
+import { access, defaultPluginOptions, type Plugin, type PluginOptions } from '@neokit-dev/core';
+import { defaultNamespace as rns, id as rid, RelationalPlugin } from '@neokit-dev/relational';
+import { D1Database } from '@cloudflare/workers-types';
+
+export const id = 'dev.neokit.d1';
+export const defaultNamespace = `${rns}-d1`;
+export const apiVersion = 2;
+export const version = 1;
+export const requires = {
+	[rid]: [1, 1]
+};
+
+export class D1Plugin extends RelationalPlugin {
+	constructor(options: D1PluginOptions) {
+		super({
+			queryFn: async (query) => (await options.db.prepare(query).bind().run()).results,
+			...options
+		});
+	}
+}
+
+export interface D1PluginOptions extends PluginOptions {
+	db: D1Database;
+	stringifyObjects?: boolean;
+	timezone?: string;
+}
+
+export function plugin(options: D1PluginOptions): Plugin {
+	return {
+		id,
+		version,
+		apiVersion,
+		plugin: new D1Plugin(options),
+		...defaultPluginOptions(options, { namespace: defaultNamespace })
+	};
+}
+
+export function query(q: string, ...p: unknown[]): Promise<Record<string, unknown>[]> {
+	return namespace(defaultNamespace).query(q, ...p);
+}
+
+export function namespace(namespace: string): D1Plugin {
+  return access(id)[namespace].plugin as D1Plugin;
+}
