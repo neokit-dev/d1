89342e5b3f12753550ffdda3ea3051008f177b18
Leonard Lesinski <84378319+Le0X8@users.noreply.github.com>
2024-12-13 20:49:59 +0100

    d1 v2

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,13 +1,13 @@
-import { access, defaultPluginOptions, type Plugin, type PluginOptions } from '@neokit-dev/core';
+import { access, defaultPluginOptions, inject, type Plugin, type PluginOptions } from '@neokit-dev/core';
 import { defaultNamespace as rns, id as rid, RelationalPlugin } from '@neokit-dev/relational';
 import type { D1Database } from '@cloudflare/workers-types';
 
 export const id = 'dev.neokit.d1';
 export const defaultNamespace = `${rns}-d1`;
-export const apiVersion = 2;
-export const version = 1;
+export const apiVersion = 3;
+export const version = 2;
 export const requires = {
-	[rid]: [1, 1]
+	[rid]: [2, 2]
 };
 
 export class D1Plugin extends RelationalPlugin {
@@ -25,22 +25,26 @@ export class D1Plugin extends RelationalPlugin {
 
 export interface D1PluginOptions extends PluginOptions {
 	db: D1Database;
+	relationalNamespace?: string;
 	stringifyObjects?: boolean;
 	timezone?: string;
 }
 
 export function plugin(options: D1PluginOptions): Plugin {
-	return {
+	const p = {
 		id,
 		version,
 		apiVersion,
 		plugin: new D1Plugin(options),
-		...defaultPluginOptions(options, { namespace: defaultNamespace })
+		requires,
+		...defaultPluginOptions(options, { namespace: defaultNamespace }),
 	};
+	inject(rid, options.relationalNamespace || rns, p.plugin);
+	return p;
 }
 
 export function query(q: string, ...p: unknown[]): Promise<Record<string, unknown>[]> {
-	return namespace(defaultNamespace).query(q, ...p);
+	return namespace(rns).query(q, ...p);
 }
 
 export function namespace(namespace: string): D1Plugin {
